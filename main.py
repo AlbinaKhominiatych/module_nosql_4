f791fd770b12ee1d81b7b89bdaed950c5e5259c7
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2024-01-31 20:33:31 +0200

    Add register_userand login

--- main.py
+++ main.py
@@ -1,63 +1,34 @@
+import bcrypt
 import redis
-import time
 
-#підключення до локального сервера
-r = redis.Redis(host='localhost', port=6379, db = 0, decode_responses=True)
-#творення числового ключа
-r.set('counter', 100)
-#інкрементування
-r.incr('counter')
-r.incr('counter')
-r.incr('counter', -101)
-#декрементування
-r.decr("counter")
-value = r.get('counter')
-print(value)
 
-# Додавання геоданих
-r.geoadd('cities', (-97.7431, 30.2672, 'Austin'))
-r.geoadd('cities', (-118.2437, 34.0522, 'Los Angeles'))
+class ShoppingCart:
+    def __init__(self):
+        self.redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 
-# Обчислення відстані між двома точками
-distance = r.geodist('cities', 'Austin', 'Los Angeles', unit='km')
-print(f"Відстань між Austin та Los Angeles: {distance} км")
+    def register_user(self, username, password):
+        #перевірити чи існує користувач
+        if self.redis_client.hexists(username, 'password_hash'):
+            print("Користувач вже існує")
+            return False
+        #створення та зберігання хешу
+        password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        self.redis_client.hset(username, 'password_hash', password_hash)
+        print("Користувач успішно зареєстрований!")
+        return True
 
-# Отримання місцеположення
-location = r.geopos('cities', 'Austin')
-print(f"Місцеположення Austin: {location}")
+    def login(self, username, password):
+        #перевірка логіну та паролю
+        stored_password_hash = self.redis_client.hget(username, 'password_hash')
+        if stored_password_hash and bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode("utf-8")):
+            print(f'Ви увійшли як {username}')
+            self.current_user = username
+            return True
+        else:
+            print("Невірний логін або пароль")
+            return False
 
-#робота з рядками
-r.set('Hello', 'World')
-print(r.get("hello"))
-
-#списки
-r.lpush('mylist', 23)
-r.lpush('mylist', 22)
-r.rpush('mylist', "one")
-print(r.lrange('mylist', 0 , -1))
-
-#множини
-r.sadd('myset', 'a', 'b', 'c')
-print(r.smembers("myset"))
-
-#хешування
-r.hset('myhash','field1', 'value')
-print(r.hget("myhash", 'field1'))
-
-print(hash("value") == hash("value"))
-
-# Функція для кешування
-def get_cached_data(key):
-    if r.exists(key):
-        print("Отримано з кешу")
-        return r.get(key)
-    else:
-        # Імітація важкої обчислювальної операції
-        time.sleep(2)
-        value = "дані"
-        r.set(key, value, ex=10)  # Зберігання в кеші з часом життя 10 секунд
-        return value
-
-# Використання функції
-print(get_cached_data('mykey'))  # Завантаження даних і зберігання в кеші
-print(get_cached_data('mykey'))  # Отримання даних з кешу
+cart_app = ShoppingCart()
+if cart_app.register_user('user4', '123'):
+    if cart_app.login('user4', '123'):
+        print("Успіх")
